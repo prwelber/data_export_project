841d3f8b1def6eab0ddea454bfbd93cbeddb4ed5
Philip Welber <pwelber@targetedsocial.com>
2016-01-27 16:15:19 -0500

    cleaer code and more efficiency

--- looping_testing.py
+++ looping_testing.py
@@ -2,7 +2,7 @@ import json
 import csv
 
 
-f = open('Constlltn_Mar_Sep_14_data_pull.json', 'r')
+f = open('Clos_du_Bois_data_pull.json', 'r')
 arr_for_csv = []
 parsed = json.load(f)
 # print(parsed['data'][0]['end_time'])
@@ -119,61 +119,68 @@ parsed = json.load(f)
 
 arr_for_csv = []
 
-
-
-for i in parsed[0]['data']:
-    new_list = []
-    gender_arr = i['targetingsentencelines']['targetingsentencelines']
-    new_list.append(i['name'])
-    new_list.append(i['start_time'][0:10])
-
+counter = 0
+while True:
     try:
-        new_list.append(i['end_time'][0:10])
-    except KeyError:
-        try:
-            new_list.append(i['insights']['data'][0]['date_stop'])
-        except KeyError:
-            new_list.append('Date Not Available')
+        for i in parsed[counter]['data']:
+            new_list = []
+            gender_arr = i['targetingsentencelines']['targetingsentencelines']
+            new_list.append(i['name'])
+            new_list.append(i['start_time'][0:10])
 
-    try:
-        new_list.append(i['targeting'])
-    except KeyError:
-        new_list.append('No Targeting Information')
+            try:
+                new_list.append(i['end_time'][0:10])
+            except KeyError:
+                try:
+                    new_list.append(i['insights']['data'][0]['date_stop'])
+                except KeyError:
+                    new_list.append('Date Not Available')
 
-    # for gender
-    try:
-        if {'content': 'Gender:', 'children': ['Female']} in gender_arr:
-            new_list.append('Female')
-        elif {'content': 'Gender:', 'children': ['Male']} in gender_arr:
-            new_list.append('Male')
-        else:
-            new_list.append('No Gender Data')
-    except KeyError:
-        new_list.append('No Gender Data Available')
+            try:
+                new_list.append(i['targeting'])
+            except KeyError:
+                new_list.append('No Targeting Information')
 
-    # This is for the account name
-    new_list.append(i['name'][0:19])
+            # for gender
+            try:
+                if {'content': 'Gender:', 'children': ['Female']} in gender_arr:
+                    new_list.append('Female')
+                elif {'content': 'Gender:', 'children': ['Male']} in gender_arr:
+                    new_list.append('Male')
+                else:
+                    new_list.append('No Gender Data')
+            except KeyError:
+                new_list.append('No Gender Data Available')
 
-    try:
-        new_list.append(i['insights']['data'][0]['spend'] * 1.15)
-    except KeyError:
-        new_list.append('Spend Not Available')
-    try:
-        new_list.append(i['targeting']['age_min'])
-    except KeyError:
-        new_list.append('No Targeting')
-    try:
-        new_list.append(i['targeting']['age_max'])
-    except KeyError:
-        new_list.append('No Targeting')
-    try:
-        new_list.append(i['targetingsentencelines'])
-    except KeyError:
-        new_list.append('No Targeting Sentence Lines')
+            # This is for the account name
+            new_list.append(i['name'][0:19])
 
-    arr_for_csv.append(new_list)
+            try:
+                new_list.append(i['insights']['data'][0]['spend'] * 1.15)
+            except KeyError:
+                new_list.append('Spend Not Available')
+            try:
+                new_list.append(i['targeting']['age_min'])
+            except KeyError:
+                new_list.append('No Targeting')
+            try:
+                new_list.append(i['targeting']['age_max'])
+            except KeyError:
+                new_list.append('No Targeting')
+            try:
+                new_list.append(i['targetingsentencelines'])
+            except KeyError:
+                new_list.append('No Targeting Sentence Lines')
 
-print(arr_for_csv)
+
+            arr_for_csv.append(new_list)
+        counter += 1
+    except IndexError:
+        break
+
+for i in arr_for_csv:
+    print(i)
+    print('\n')
 
 
 
--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import csv
 # Get adset targeting through acct num
 # targetingsentencelines gives more depth
 
-act_num = 108294695945331
-act_name = 'Constlltn_Mar_Sep_14'
+act_num = 813429925373770
+act_name = 'Clos_du_Bois'
 
 def get_data(act_id, act_name):
     my_access_token = 'CAAN4vFUE2ZAgBAHaZA6dmP6v4eIxOcV8TtA2crGjLG47ZCEllpjUSUlGFGDIFCX0KQrWBw8OGY9I7vi087ekgpaoldSyaya3HtIJgzC7oR2GQnpE8TfWi8uAB7LqtjMGqtgmvzFXZBTytZCkMDVm9WTC9vBqQZAuxVpj10yyQZC0WigZBaxvKfvG'
@@ -46,67 +46,75 @@ def get_data(act_id, act_name):
 
 """ this is for paginated data from facebook """
 
+"""
+What's happening here is first we're referencing the length of the account name to use to slice a string. Then we're opening the json file that was written in the previous function. then we're opening an empty array which will eventually be written to a csv file. Then we parse the JSON. Then we set a counter to zero and enter a while loop. In the while loop we try a for loop over the parsed JSON. We use the counter variable to reference an index in the parsed JSON which is just a big list. We begin each loop with a new empty array. We try/except various data points and at the end of each loop we append the [new_list] to the [arr_for_csv] that we created at the beginning of the function. When the for loop is over, the counter increases by one and the while loop, which is running because we set it to True, will run another for loop, but with an increased counter (index value), and so on. When the index throws an error, we break out of the Whlie loops. Then we open to the csv and write to it, looping over the [arr_for_csv] we just created with the while and for loops. arr_for_csv is a list with lots of lists inside it. each interior list will be a row.
+"""
+
 def paginated_json_to_csv(file):
     name_len = len(act_name)
     f = open(file, 'r')
     arr_for_csv = []
     parsed = json.load(f)
+    counter = 0
+    while True:
+        try:
+            for i in parsed[counter]['data']:
+                new_list = []
+                gender_arr = i['targetingsentencelines']['targetingsentencelines']
+                new_list.append(i['name'])
+                new_list.append(i['start_time'][0:10])
 
-    # Need to reset the index to zero every time this function runs
-    for i in parsed[11]['data']:
-        new_list = []
-        gender_arr = i['targetingsentencelines']['targetingsentencelines']
-        new_list.append(i['name'])
-        new_list.append(i['start_time'][0:10])
+                try:
+                    new_list.append(i['end_time'][0:10])
+                except KeyError:
+                    try:
+                        new_list.append(i['insights']['data'][0]['date_stop'])
+                    except KeyError:
+                        new_list.append('Date Not Available')
 
-        try:
-            new_list.append(i['end_time'][0:10])
-        except KeyError:
-            try:
-                new_list.append(i['insights']['data'][0]['date_stop'])
-            except KeyError:
-                new_list.append('Date Not Available')
+                try:
+                    new_list.append(i['targeting'])
+                except KeyError:
+                    new_list.append('No Targeting Information')
 
-        try:
-            new_list.append(i['targeting'])
-        except KeyError:
-            new_list.append('No Targeting Information')
+                # for gender
+                try:
+                    if {'content': 'Gender:', 'children': ['Female']} in gender_arr:
+                        new_list.append('Female')
+                    elif {'content': 'Gender:', 'children': ['Male']} in gender_arr:
+                        new_list.append('Male')
+                    else:
+                        new_list.append('No Gender Data')
+                except KeyError:
+                    new_list.append('No Gender Data Available')
 
-        # for gender
-        try:
-            if {'content': 'Gender:', 'children': ['Female']} in gender_arr:
-                new_list.append('Female')
-            elif {'content': 'Gender:', 'children': ['Male']} in gender_arr:
-                new_list.append('Male')
-            else:
-                new_list.append('No Gender Data')
-        except KeyError:
-            new_list.append('No Gender Data Available')
+                # This is for the account name
+                new_list.append(i['name'][0:name_len])
 
-        # This is for the account name
-        new_list.append(i['name'][0:name_len])
+                try:
+                    new_list.append(i['insights']['data'][0]['spend'] * 1.15)
+                except KeyError:
+                    new_list.append('Spend Not Available')
+                try:
+                    new_list.append(i['targeting']['age_min'])
+                except KeyError:
+                    new_list.append('No Targeting')
+                try:
+                    new_list.append(i['targeting']['age_max'])
+                except KeyError:
+                    new_list.append('No Targeting')
+                try:
+                    new_list.append(i['targetingsentencelines'])
+                except KeyError:
+                    new_list.append('No Targeting Sentence Lines')
 
-        try:
-            new_list.append(i['insights']['data'][0]['spend'] * 1.15)
-        except KeyError:
-            new_list.append('Spend Not Available')
-        try:
-            new_list.append(i['targeting']['age_min'])
-        except KeyError:
-            new_list.append('No Targeting')
-        try:
-            new_list.append(i['targeting']['age_max'])
-        except KeyError:
-            new_list.append('No Targeting')
-        try:
-            new_list.append(i['targetingsentencelines'])
-        except KeyError:
-            new_list.append('No Targeting Sentence Lines')
-        print('completed loop')
-        arr_for_csv.append(new_list)
+                arr_for_csv.append(new_list)
+            counter += 1
+        except IndexError:
+            break
 
     # open CSV and get ready to append stuff
-    with open('master.csv', 'a', newline='', encoding='utf-8') as f:
+    with open('test.csv', 'a', newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
         for i in arr_for_csv:
             writer.writerow(i)
@@ -114,4 +122,4 @@ def paginated_json_to_csv(file):
     f.close()
     return arr_for_csv
 
-# paginated_json_to_csv('Constlltn_Mar_Sep_14_data_pull.json')
+paginated_json_to_csv('Clos_du_Bois_data_pull.json')
